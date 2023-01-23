7a333a5a061c0d0703c5bb58c70e6c8df86c39ef
BM-laoli <18376621755@163.com>
2023-01-23 20:57:31 +0800

    http grpc同时支持

---
+++ client/simple_http_client/client.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	pb "github.com/BM-laoli/go-gin-example/proto"
+)
+
+const PORT = "9001"
+
+func main() {
+	certFile := "../../conf/client_cert.pem"
+	keyFile := "../../conf/client_key.pem"
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+
+	if err != nil {
+		log.Fatalf("tls.LoadX509KeyPair err: %v", err)
+	}
+
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile("../../conf/ca_cert.pem")
+	if err != nil {
+		log.Fatalf("ioutil.ReadFile err: %v", err)
+	}
+
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		log.Fatalf("certPool.AppendCertsFromPEM err")
+	}
+
+	c := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ServerName:   "x.test.example.com",
+		RootCAs:      certPool,
+	})
+
+	conn, err := grpc.Dial(":"+PORT, grpc.WithTransportCredentials(c))
+	if err != nil {
+		log.Fatalf("grpc.Dial err: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewSearchServiceClient(conn)
+	resp, err := client.Search(context.Background(), &pb.SearchRequest{
+		Request: "gRPC http",
+	})
+	if err != nil {
+		log.Fatalf("client.Search err: %v", err)
+	}
+
+	log.Printf("resp: %s", resp.GetResponse())
+}
---
+++ server/simple_http_server/server.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"runtime/debug"
+	"strings"
+
+	pb "github.com/BM-laoli/go-gin-example/proto"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
+)
+
+type SearchService struct {
+	pb.SearchServiceServer
+}
+
+// 直接抓进来 实现？集成？还是用的 实现 来说比较合适吧
+
+func (s *SearchService) Search(ctx context.Context, r *pb.SearchRequest) (*pb.SearchResponse, error) {
+	return &pb.SearchResponse{Response: r.GetRequest() + " Server!!!"}, nil
+}
+
+const PORT = "9001"
+
+func LoggingInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	log.Printf("gRPC method: %s, %v", info.FullMethod, req)
+	resp, err := handler(ctx, req)
+	log.Printf("gRPC method: %s, %v", info.FullMethod, resp)
+	return resp, err
+}
+
+func RecoveryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			debug.PrintStack()
+			err = status.Errorf(codes.Internal, "Panic err: %v", e)
+		}
+	}()
+
+	return handler(ctx, req)
+}
+
+func main() {
+	// 测试证书
+	certFile := "../../conf/server_cert.pem"
+	keyFile := "../../conf/server_key.pem"
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+
+	if err != nil {
+		log.Fatalf("tls.LoadX509KeyPair err: %v", err)
+	}
+
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile("../../conf/client_ca_cert.pem")
+	if err != nil {
+		log.Fatalf("ioutil.ReadFile err: %v", err)
+	}
+
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		log.Fatalf("certPool.AppendCertsFromPEM err")
+	}
+
+	c := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    certPool,
+	})
+
+	opts := []grpc.ServerOption{
+		grpc.Creds(c),
+		grpc_middleware.WithUnaryServerChain(
+			RecoveryInterceptor,
+			LoggingInterceptor,
+		),
+	}
+
+	// 开启测试
+	mux := GetHTTPServeMux()
+
+	server := grpc.NewServer(opts...)
+	pb.RegisterSearchServiceServer(server, &SearchService{})
+
+	http.ListenAndServeTLS(":"+PORT,
+		certFile,
+		keyFile,
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+				server.ServeHTTP(w, r)
+			} else {
+				mux.ServeHTTP(w, r)
+			}
+
+			return
+		}),
+	)
+}
+
+// 开启支持 http/https
+func GetHTTPServeMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("bm-laoli: go-inxxxxx"))
+	})
+	return mux
+}
